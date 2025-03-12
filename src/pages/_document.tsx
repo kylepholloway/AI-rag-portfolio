1d851ebe2c567380a572379b858149e7847f90e7
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2025-03-12 16:30:02 -0600

    update anaylics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Document() {
   return (
@@ -13,8 +11,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
-        <SpeedInsights />
       </body>
     </Html>
   );
